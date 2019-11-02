8d5577a32146bf4d4a3b256f57269e3a15a5caa2
fleur101 <gaukhard96@gmail.com>
2019-11-02 21:49:46 +0200

    Init commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+require('./db/mongoose')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
+
+
+const Task = require('./models/task')
+const User = require('./models/user')
+const main = async () => {
+    // const task = await Task.findById('5dbdd4501ba7694a9c180ce6')
+    // await task.populate('owner').execPopulate()
+    // console.log(task.owner)
+
+    const user = await User.findById('5dbdd359db54b014ec066816')
+    await user.populate('tasks').execPopulate()
+    console.log(user.tasks)
+}
+
+main()
